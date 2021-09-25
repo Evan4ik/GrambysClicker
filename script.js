7d33e75a41da93193bd81d6996ce0b0c812bdf50
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-09-25 11:35:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //THE SMOOTH TASTE OF NEO!!
 console.log("NEO")
+var money = 0
+var mps = 5
+var i = false
+var moneyCalc = 0
+function moneyCalc() {
+  money += mps
+  setTimeout(() => {
+    moneyCalc()
+  }, 1)
+  if (!i) {
+    i = true
+    setTimeout(() => {
+       moneyCalc = money
+       i = false
+       console.log(moneyCalc)
+    }, 1000)
+  }
+}
