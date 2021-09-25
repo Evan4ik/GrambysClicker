5b35a962d0e7ccfd2efb402d808a8915ec1d295d
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-09-25 16:54:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-//THE SMOOTH TASTE OF NEO!!
-console.log("NEO")
 var data = localStorage.gcData
 var miles = 0
-var mps = 0.01
-var i = false
+var mps = 0
 if (data == null) {
 	localStorage.gcData = [miles, mps]
 	localStorage.gcData = CompressBin(localStorage.gcData)
 	data = localStorage.gcData.split(",")
 	console.log(data)
 }
-data = [0, 0.1]
-localStorage.gcData = CompressBin(data)
-data = localStorage.gcData
-console.log(data)
-localStorage.gcData = UncompressBin(data)
-data = localStorage.gcData
-console.log(data)
-moneyCalc() 
+
+data = [0, 0]
+localStorage.gcData = data
+
 
 function moneyCalc() {
   miles += mps
@@ -41,19 +34,4 @@ function CompressBin(arr)//compress a sequence like [0,1,1,0,1,0]... into a numb
 	}
 	str=parseInt(str,2);
 	return str;
-}
-
-function UncompressBin(num)//uncompress a number like 54 to a sequence like [0,1,1,0,1,0].
-{
-	var arr=num.toString(2);
-	console.log(arr)
-	console.log(num)
-	arr=arr.split('');
-	arr.reverse();
-	console.log(arr)
-	arr.shift();
-	console.log(arr)
-	arr.pop();
-	console.log(arr)
-	return arr;
 }
