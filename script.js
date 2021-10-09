9a6e926a435b45fe5e15cd67666589e3d07b1b67
Evan4ik <64590088+Evan4ik@users.noreply.github.com>
2021-10-09 13:30:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function background() {
  }
   if (document.getElementById("sign").style.display == "block") {
 	  let pos = document.getElementById("sign").style.left
-	  pos = split("px")
+	  pos = pos.split("px")
 	  pos = parseInt(pos[0])
 	  pos -= 10
 	  document.getElementById("sign").style.left = pos + "px"	  
